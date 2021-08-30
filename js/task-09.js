a8fcaec3da39068b191a93c480239b001e7b0b39
Alexander Osadchuk <85343271+Justice1505@users.noreply.github.com>
2021-08-30 21:16:26 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,8 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const bodyElement = document.querySelector("body");
-
 const colors = {
   body: document.querySelector("body"),
   btn: document.querySelector(".change-color"),
@@ -15,6 +13,6 @@ colors.btn.addEventListener("click", changeColor);
 function changeColor() {
   const color = getRandomHexColor();
 
-  bodyElement.style.backgroundColor = color;
+  colors.body.style.backgroundColor = color;
   colors.color.textContent = color;
 }
